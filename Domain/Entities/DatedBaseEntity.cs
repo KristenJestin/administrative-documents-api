bdb8b93d3a35ceb6fabf7c48a2daf62826bcf7ee
Kristen Jestin <kris.jestin@laposte.net>
2021-01-11 16:48:06 +0100

    feat: add base entities

---
+++ Domain/Entities/DatedBaseEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Domain.Entities
+{
+    public abstract class DatedBaseEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; }
+        public string CreatedBy { get; set; }
+        public DateTime? UpdatedAt { get; set; }
+        public string UpdatedBy { get; set; }
+    }
+}
