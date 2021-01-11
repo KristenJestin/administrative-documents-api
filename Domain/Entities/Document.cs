1f7e17433d56d7c05ee00cbc213ab4b881daddd5
Kristen Jestin <kris.jestin@laposte.net>
2021-01-11 17:32:27 +0100

    feat: add default app models

---
+++ Domain/Entities/Document.cs
@@ -0,0 +1,22 @@
+﻿using Domain.Common;
+using System;
+
+namespace Domain.Entities
+{
+    public class Document: DatedBaseEntity
+    {
+        public string Name { get; set; }
+        public DocumentFile File { get; set; }
+        public string Note { get; set; }
+        public DocumentType Type { get; set; }
+        public Folder Folder { get; set; }
+
+
+        #region driven by type
+        public decimal Amount { get; set; }
+        public DateTime? Date { get; set; }
+        public int? Duration { get; set; }
+        public DateTime? EndDate { get; set; }
+        #endregion
+    }
+}
---
+++ Domain/Entities/DocumentFile.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Common;
+
+namespace Domain.Entities
+{
+    public class DocumentFile : DatedBaseEntity
+    {
+        public string OriginalName { get; set; }
+        public string Path { get; set; }
+        public string Encryption { get; set; }
+        public long Size { get; set; }
+        public Document Document { get; set; }
+    }
+}
